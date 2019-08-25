27bee8baf56f3c7b1fdee2c5f5ed1cb79125fc30
James Gibbel <jamesgibbel@gmail.com>
2019-08-25 23:29:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,21 @@
 # Your Code Here
 
-def map(source_array)
+def map2(source_array)
   new = []
   for x in 0..source_array.length-1 do 
       new.push(yield source_array[x])
   end
   p new
 end
+
+def map(source_array)
+  new = []
+  source_array.each do |x|
+    new << yield(x)
+  end
+  new
+end
+
 
 map([1, 2, 3, -9]) { |n| n*n}
 
